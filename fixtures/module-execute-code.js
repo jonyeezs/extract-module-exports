3cecf04133f545468d55f40d17e995fe4ee408c9
Jonathan Yee <jonyeezs@gmail.com>
2019-02-06 15:35:04 +1000

    Adde test cases

---
+++ fixtures/module-execute-code.js
@@ -0,0 +1,7 @@
+// Used to ensure file lookups do not execute code
+
+global.outsideTestCase = true;
+
+module.exports = function() {
+    global.insideTestCase = true;
+};
---
+++ fixtures/module-variable.js
@@ -0,0 +1,3 @@
+const ref = { a: "a", b: function() {} };
+
+module.exports = ref;
